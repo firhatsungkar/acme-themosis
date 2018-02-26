cb788c7f9ee7f3d340110b4fc9fbd74d753ee4a4
Muhamad Firhat <muhamad.firhat@gmail.com>
2018-02-26 18:31:35 +0700

    register model namespace

--- htdocs/content/plugins/storyware-donations/resources/config/us_storyware_donations_loading.config.php
+++ htdocs/content/plugins/storyware-donations/resources/config/us_storyware_donations_loading.config.php
@@ -5,5 +5,6 @@
  */
 return [
     'Us\\Storyware\\Donations\\Services\\' => themosis_path('plugin.us.storyware.donations.resources').'providers',
+    'Us\\Storyware\\Donations\\Models\\' => themosis_path('plugin.us.storyware.donations.resources').'models',
     'Us\\Storyware\\Donations\\Controllers\\' => themosis_path('plugin.us.storyware.donations.resources').'controllers'
 ];
