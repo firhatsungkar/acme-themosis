a649faef18fc2a13a21c46eed93ed9c8d7b6ed60
Muhamad Firhat <muhamad.firhat@gmail.com>
2018-02-26 18:32:09 +0700

    add Member model

---
+++ htdocs/content/plugins/storyware-donations/resources/models/Member.php
@@ -0,0 +1,10 @@
+<?php
+namespace Us\Storyware\Donations\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model {
+  protected $table = 'members';
+  protected $primaryKey = 'id';
+  
+}
