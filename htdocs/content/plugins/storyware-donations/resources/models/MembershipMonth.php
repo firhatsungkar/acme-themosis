adf1b08ef0dc223e308f45e14d3a1beff32c5a43
Muhamad Firhat <muhamad.firhat@gmail.com>
2018-02-28 18:44:39 +0700

    add MembershipMonth model

---
+++ htdocs/content/plugins/storyware-donations/resources/models/MembershipMonth.php
@@ -0,0 +1,10 @@
+<?php
+namespace Us\Storyware\Donations\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MembershipMonth extends Model {
+  protected $table = 'membership_months';
+  protected $primaryKey = 'id';
+  
+}
