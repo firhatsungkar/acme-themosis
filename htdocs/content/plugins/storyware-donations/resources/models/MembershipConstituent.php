931ffcb8fb2eff574547659eb66f0ab1a834db30
Muhamad Firhat <muhamad.firhat@gmail.com>
2018-02-28 18:44:59 +0700

    add MembershipConstituent model

---
+++ htdocs/content/plugins/storyware-donations/resources/models/MembershipConstituent.php
@@ -0,0 +1,10 @@
+<?php
+namespace Us\Storyware\Donations\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MembershipConstituent extends Model {
+  protected $table = 'membership_constituents';
+  protected $primaryKey = 'id';
+  
+}
