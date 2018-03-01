0f28f8a37e5ac56933d79592f07209c0e8ad1530
Muhamad Firhat <muhamad.firhat@gmail.com>
2018-03-01 16:49:59 +0700

    add gift relation and some accessor

--- htdocs/content/plugins/storyware-donations/resources/models/MembershipMonth.php
+++ htdocs/content/plugins/storyware-donations/resources/models/MembershipMonth.php
@@ -2,9 +2,51 @@
 namespace Us\Storyware\Donations\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
 class MembershipMonth extends Model {
   protected $table = 'membership_months';
   protected $primaryKey = 'id';
+
+  public function gifts()
+  {
+    return $this->hasMany(MonthlyGift::class, 'membership_month_id', 'id');
+  }
+
+  public function getExpectedGiftsAttribute()
+  {
+    return $this->calculateExpectedGifts($this->id);
+  }
+
+  public function getReceivedGiftsAttribute()
+  {
+    return $this->calculateReceivedGifts($this->id);
+  }
+
+  private function calculateExpectedGifts($id)
+  {
+    return $this->with('gifts')
+                ->where('id', $id)
+                ->first()
+                ->gifts
+                ->map(function($params, $index) {
+                  return $params->expected_amount;
+                })
+                ->reduce(function($carry, $amount){
+                  return $carry + $amount;
+                }, 0);
+  }
+
+  private function calculateReceivedGifts($id)
+  {
+    return $this->with('gifts')
+                ->where('id', $id)
+                ->first()
+                ->gifts
+                ->map(function($params, $index) {
+                  return $params->received_amount;
+                })
+                ->reduce(function($carry, $amount){
+                  return $carry + $amount;
+                }, 0);
+  }
   
 }
