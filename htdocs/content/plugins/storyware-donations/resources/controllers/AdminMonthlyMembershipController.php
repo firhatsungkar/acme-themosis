7774300a2030cfcce7afb1281ea47a36b872132d
Muhamad Firhat <muhamad.firhat@gmail.com>
2018-03-01 16:42:03 +0700

    load MonthlyMembership

--- htdocs/content/plugins/storyware-donations/resources/controllers/AdminMonthlyMembershipController.php
+++ htdocs/content/plugins/storyware-donations/resources/controllers/AdminMonthlyMembershipController.php
@@ -7,12 +7,21 @@ use Themosis\Facades\View;
 use Us\Storyware\Donations\Models\MembershipMonth;
 
 
-class AdminMonthlyMembershipController extends BaseController {
+class AdminMonthlyMembershipController extends AdminController {
   /**
    * @var MembershipMonth
    */
   protected $membershipMonth;
-  
+
+  /** Default params
+   * @var array
+   */
+  protected $default = [
+    'item_per_page' => 10,
+    'order' => 'asc',
+    'orderBy' => 'id',
+  ];
+
   /**
    * AdminMonthlyMembershipController Constructor
    *
@@ -29,9 +38,32 @@ class AdminMonthlyMembershipController extends BaseController {
    * @param array $params
    * @return View
    */
-  public function index($params = [])
+  public function index()
   {
-    return View::make('sample');
+    // Get params from parent
+    $params = $this->params;
+    // Get query
+    $query = $this->getQuery();
+    
+    // Get Uri
+    $url = $this->getUrl();
+    $sortUrl = $this->getUrl(true);
+    
+    // Set default value
+    $itemPerPage = $this->getParam($params, 'item_per_page', $this->default['item_per_page']);
+    $order = $this->getParam($query, 'order', $this->default['order']);
+    $orderBy = $this->getParam($query, 'orderBy', $this->default['orderBy']);
+    
+    // Sort members
+    $members = $this->membershipMonth->with('gifts')->orderBy($orderBy, $order);
+
+    // Get members with pagination
+    $members = $members->paginate($itemPerPage);
+
+    return View::make(
+      'us.storyware-donations.monthly-membership.index',
+      compact('members', 'order', 'orderBy', 'url', 'sortUrl')
+    );
   }
   
 }
