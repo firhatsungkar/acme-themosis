b3bb4a68de359a1e9746b508082eb6042d40431f
Muhamad Firhat <muhamad.firhat@gmail.com>
2018-03-03 08:03:06 +0700

    clean up

--- htdocs/content/plugins/storyware-donations/resources/admin/plugin.php
+++ htdocs/content/plugins/storyware-donations/resources/admin/plugin.php
@@ -16,58 +16,55 @@ use Us\Storyware\Donations\Controllers\AdminConstituentMatchingController;
  * Write your plugin custom code below.
  */
 
-// Get All Request Input
+/** Get All Request Input */
 $request = Input::all();
 
-// Initiate Model
+/** Initiate Model */
 $model = [
   'member' => new Member(),
   'membershipMonth' => new MembershipMonth(),
   'membershipConstituent' => new MembershipConstituent(),
 ];
 
-// Initiate BaseController
+/** Initiate BaseController */
 $baseController = new AdminController($request);
 
-// Initiate Controller
+/** Initiate Controller */
 $controller = [
   'donations'         => new AdminDonationsController( $model['member'] ),
   'monthlyMembership' => new AdminMonthlyMembershipController( $model['membershipMonth'] ),
   'constituentMatching' => new AdminConstituentMatchingController( $model['membershipConstituent'] ),
 ];
 
-// Define The Page
-$donation = Page::make(
-              'storyware-donations',
-              'All Donations',
-              null,
-              $controller['donations']->index()
-            )
-            ->set([
-              'capability' => 'manage_options',
-              'icon' => 'dashicons-archive',
-              'position' => 20,
-              'tabs' => true,
-              'menu' => __("Donations")
-            ]);
+/** Define The Page */
+$donation = Page::make(               /** Page Settings */
+  'storyware-donations',                // Page slug
+  'All Donations',                      // Page Title
+  null,                                 // Parent Page (null = root)
+  $controller['donations']->index()     // Controller of page
+)->set([                              /** Menu Settings */
+  'capability' => 'manage_options',     // User capabilty
+  'icon' => 'dashicons-archive',        // Menu Icon
+  'position' => 20,                     // Menu Position
+  'tabs' => true,                       // Support tabs
+  'menu' => __("Donations")             // Menu Title
+]);
 
-// Define Sub Menu of storyware-donations
+/** Define Sub Menu of storyware-donations */
 $monthlyMembership = Page::make(
-              'storyware-monthly-membership',
-              'Monthly Membership',
-              'storyware-donations',
-              $controller['monthlyMembership']->index()
-            )
-            ->set([
-              'menu' => __("Monthly Membership")
-            ]);
+  'storyware-monthly-membership',
+  'Monthly Membership',
+  'storyware-donations',
+  $controller['monthlyMembership']->index()
+)->set([
+  'menu' => __("Monthly Membership")
+]);
 
 $constituentMatching = Page::make(
-              'storyware-constituent-matching',
-              'Constituent Matching',
-              'storyware-donations',
-              $controller['constituentMatching']->index()
-            )
-            ->set([
-              'menu' => __("Constituent Matching")
-            ]);
+  'storyware-constituent-matching',
+  'Constituent Matching',
+  'storyware-donations',
+  $controller['constituentMatching']->index()
+)->set([
+  'menu' => __("Constituent Matching")
+]);
